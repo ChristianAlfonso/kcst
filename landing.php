cabc1d72ecd4c4ed396452880b4b0c40ca054b6a
Christian Alfonso <alfonso.christian.macatangay@gmail.com>
2024-12-09 11:24:52 +0800

    new

--- landing.php
+++ landing.php
@@ -480,6 +480,12 @@ $result_announcements = $conn->query($sql_announcements);
                 bsOffcanvas.hide();
             });
         });
+
+        // Add fade-up class to #demo section
+        document.addEventListener('DOMContentLoaded', function() {
+            document.getElementById('demo').classList.add('fade-up');
+        });
+        
     </script>
     <script src="faqs.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
